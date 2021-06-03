df78227c656b68f1781e10e30071404153ae2439
ndhuynh <ndhuynh@deakin.edu.au>
2021-06-03 14:36:34 +1000

    add

---
+++ submission/deakin_ai_challenge_submission.py
@@ -0,0 +1,92 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Copyright (c) 2020. Reda Bouadjenek, Deakin University                      +
+#     Email:  reda.bouadjenek@deakin.edu.au                                    +
+#                                                                              +
+#  Licensed under the Apache License, Version 2.0 (the "License");             +
+#   you may not use this file except in compliance with the License.           +
+#    You may obtain a copy of the License at:                                  +
+#                                                                              +
+#                 http://www.apache.org/licenses/LICENSE-2.0                   +
+#                                                                              +
+#    Unless required by applicable law or agreed to in writing, software       +
+#    distributed under the License is distributed on an "AS IS" BASIS,         +
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  +
+#    See the License for the specific language governing permissions and       +
+#    limitations under the License.                                            +
+#                                                                              +
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+import sys, os, h5py
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing import image
+from tensorflow.keras import models
+from tensorflow.python.keras.saving import hdf5_format
+from tensorflow.keras import layers
+from tensorflow import keras
+
+if __name__=="__main__":
+    if len(sys.argv)==1:
+        input_dir = '.'
+        output_dir = '.'
+    else:
+        input_dir = os.path.abspath(sys.argv[1])
+        output_dir = os.path.abspath(sys.argv[2])
+        
+    print("Using input_dir: " + input_dir)
+    print("Using output_dir: " + output_dir)
+    print(sys.version)
+    print("Tensorflow version: " + tf.__version__)
+
+    # Loading the model.
+    model_names = ["densenet121.h5",
+                   "densenet169.h5",
+                   "densenet201_1.h5",
+                   "densenet201_2.h5"]
+    models = []
+    for model_name in model_names:
+        with h5py.File(model_name, mode='r') as f:
+            models += [hdf5_format.load_model_from_hdf5(f)]
+
+    class_names = ['abraham_grampa_simpson', 'apu_nahasapeemapetilon', 
+        'bart_simpson', 'charles_montgomery_burns', 'chief_wiggum', 'comic_book_guy', 
+        'edna_krabappel', 'homer_simpson', 'kent_brockman', 'krusty_the_clown', 
+        'lenny_leonard', 'lisa_simpson', 'marge_simpson', 'mayor_quimby', 
+        'milhouse_van_houten', 'moe_szyslak', 'ned_flanders', 'nelson_muntz', 
+        'principal_skinner', 'sideshow_bob'
+    ]
+    input_shape = models[0].layers[0].input_shape
+    if isinstance(input_shape, list):
+        image_size = np.array(input_shape[0][1:3])
+    else:
+        image_size = np.array(input_shape[1:3])
+
+    print('Size of inputs images: ' + str(image_size))
+    # Reading test images.    
+    files = []
+    images = []
+    for file in os.listdir(input_dir):
+        if file.endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            img = image.load_img(os.path.join(input_dir, file), 
+                target_size=image_size)
+            x = image.img_to_array(img)
+            x = tf.keras.applications.densenet.preprocess_input(x)
+            x = np.expand_dims(x, axis=0)
+            images.append(x)
+            files.append(file)
+    images = np.vstack(images)
+    
+    # Making predictions!
+    batch_size = 16
+    y_proba = None
+    for model in models:
+        if y_proba is None:
+            y_proba = model.predict(images, batch_size=batch_size)
+        else:
+            y_proba += model.predict(images, batch_size=batch_size)
+    y_predict = np.argmax(y_proba,axis=1)
+    # Writting predictions to file.
+    with open(os.path.join(output_dir, 'answer.txt'), 'w') as result_file:
+        for i in range(len(files)):    
+            result_file.write(files[i] + ',' + class_names[y_predict[i]] + '\n')
